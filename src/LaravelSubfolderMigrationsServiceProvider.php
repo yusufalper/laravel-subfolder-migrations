e37e78444171bab4908480e5e8d5d8ac1cdacb44
Alper <yusufalpersari@gmail.com>
2022-09-06 14:27:40 +0300

    add package main functionnality => 'LaravelSubFolderMigrationsServiceProvider.php'

--- src/LaravelSubfolderMigrationsServiceProvider.php
+++ src/LaravelSubfolderMigrationsServiceProvider.php
@@ -4,22 +4,41 @@ namespace Yusufalper\LaravelSubfolderMigrations;
 
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use Yusufalper\LaravelSubfolderMigrations\Commands\LaravelSubfolderMigrationsCommand;
 
 class LaravelSubfolderMigrationsServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
-        $package
-            ->name('laravel-subfolder-migrations')
-            ->hasConfigFile()
-            ->hasViews()
-            ->hasMigration('create_laravel-subfolder-migrations_table')
-            ->hasCommand(LaravelSubfolderMigrationsCommand::class);
+        $package->name('laravel-subfolder-migrations');
+
+        $mainMigrationPath = database_path('migrations');
+        $directories = glob($mainMigrationPath . '/*' , GLOB_ONLYDIR);
+        $allSubDirs = self::recursiveSearch($directories);
+
+        $allMigrationPaths = array_merge([$mainMigrationPath], $allSubDirs);
+
+        $this->loadMigrationsFrom($allMigrationPaths);
+    }
+
+    protected static function recursiveSearch(array $directories): array
+    {
+        $subs = [];
+        $deepSubs = [];
+        if (count($directories) > 0){
+            $subs = self::getSubDirectories($directories);
+        }
+        if (count($subs) > 0){
+            $deepSubs = self::recursiveSearch($subs);
+        }
+        return array_merge($directories, $subs, $deepSubs);
+    }
+
+    protected static function getSubDirectories(array $directories): array
+    {
+        $subDirectories = [];
+        foreach ($directories as $directory){
+            $subDirectories[] = glob($directory . '/*' , GLOB_ONLYDIR);
+        }
+        return array_merge(...$subDirectories);
     }
 }
